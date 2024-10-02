a04c672c9eab3cc356e185cb0f3cae170b491877
clarencejeddmaas <clarencemaas74@gmail.com>
2024-10-02 22:27:39 +0800

    show the video frame with detected emotions

--- real_time_emotion_detector.py
+++ real_time_emotion_detector.py
@@ -56,3 +56,6 @@ def display_emotion_scores(video_frame, face_x, face_y, face_height, emotion_sco
         cv2.putText(video_frame, f'{emotion_label.capitalize()}: {emotion_score:.2f}', 
                     (face_x, emotion_text_y_position), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
         emotion_text_y_position += 15
+
+# 8. Show the video frame with the emotion detection
+    cv2.imshow('Emotion Detection', video_frame)
